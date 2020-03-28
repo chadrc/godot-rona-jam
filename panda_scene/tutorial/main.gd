f9d14a7430f65c4e36f5525580da56519bc78649
Chad Collins <chadrcollins91@gmail.com>
2020-03-28 18:08:58 -0400

    Finished tutorial. Began work on tower defense mechanics.

---
+++ panda_scene/tutorial/main.gd
@@ -0,0 +1,50 @@
+extends Node
+
+export (PackedScene) var Mob
+var score
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	randomize()
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	$HUD.show_game_over()
+	
+
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+	$HUD.update_score(score)
+	$HUD.show_message("Get Ready!")
+
+
+func _on_StartTimer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
+
+
+func _on_ScoreTimer_timeout():
+	score += 1
+	$HUD.update_score(score)
+
+
+func _on_MobTimer_timeout():
+	$MobPath/MobSpawnLocation.offset = randi()
+	
+	var mob = Mob.instance()
+	$HUD.connect("start_game", mob, "_on_start_game")
+	add_child(mob)
+	
+	var direction = $MobPath/MobSpawnLocation.rotation + PI / 2
+	
+	mob.position = $MobPath/MobSpawnLocation.position
+	
+	direction += rand_range(-PI / 4, PI / 4)
+	mob.rotation = direction
+	
+	mob.linear_velocity = Vector2(rand_range(mob.min_speed, mob.max_speed), 0)
+	mob.linear_velocity = mob.linear_velocity.rotated(direction)
