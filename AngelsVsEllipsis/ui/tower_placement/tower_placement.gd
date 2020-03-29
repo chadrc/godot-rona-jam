92f294b2879046ec44b18415419b1e5e32d86fcc
Chris bruno <chris234k@gmail.com>
2020-03-29 14:49:28 -0400

    Tapping the center plus button opens the menu

--- AngelsVsEllipsis/ui/tower_placement/tower_placement.gd
+++ AngelsVsEllipsis/ui/tower_placement/tower_placement.gd
@@ -4,6 +4,12 @@ var is_showing_menu
 
 onready var plus_sign = $Label
 
+# towers
+onready var tl_tower = $tl_tower
+onready var tr_tower = $tr_tower
+onready var br_tower = $br_tower
+onready var bl_tower = $bl_tower
+
 func _ready():
 	toggle_mode = true
 	
@@ -15,4 +21,9 @@ func _toggled(button_pressed):
 	set_menu_state(is_showing_menu)
 
 func set_menu_state(show):
-	plus_sign.visible = !show;
+	plus_sign.visible = !show
+	
+	tl_tower.visible = show
+	tr_tower.visible = show
+	bl_tower.visible = show
+	br_tower.visible = show
