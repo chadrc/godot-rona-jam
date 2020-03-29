56d11b4cdc5ec5ad1aaebc51d5dba8379cabe260
Chris bruno <chris234k@gmail.com>
2020-03-29 15:11:50 -0400

    Static typing is pretty cool!

--- AngelsVsEllipsis/ui/tower_placement/tower_placement.gd
+++ AngelsVsEllipsis/ui/tower_placement/tower_placement.gd
@@ -1,14 +1,15 @@
 extends Button
+class_name TowerPlacement
 
-var is_showing_menu
+var is_showing_menu := false
 
-onready var plus_sign = $Label
+onready var plus_sign: Label = $Label
 
 # towers
-onready var tl_tower = $tl_tower
-onready var tr_tower = $tr_tower
-onready var br_tower = $br_tower
-onready var bl_tower = $bl_tower
+onready var tl_tower: Button = $tl_tower
+onready var tr_tower: Button = $tr_tower
+onready var br_tower: Button = $br_tower
+onready var bl_tower: Button = $bl_tower
 
 func _ready():
 	toggle_mode = true
@@ -16,11 +17,11 @@ func _ready():
 	is_showing_menu = false
 	set_menu_state(is_showing_menu)
 
-func _toggled(button_pressed):
+func _toggled(button_pressed: bool):
 	is_showing_menu = button_pressed
 	set_menu_state(is_showing_menu)
 
-func set_menu_state(show):
+func set_menu_state(show: bool):
 	plus_sign.visible = !show
 	
 	tl_tower.visible = show
