a4984ae7744a28b2a51e293db6a3982d529effd6
Chris bruno <chris234k@gmail.com>
2020-03-29 12:49:27 -0400

    Enemy structure, basic health functionality

--- AngelsVsEllipsis/enemies/Enemy.gd
+++ AngelsVsEllipsis/enemies/Enemy.gd
@@ -1,12 +1,31 @@
-extends PathFollow2D
+extends Node2D
 
-export var speed = 50
+export (float) var move_speed = 50
 
+var max_health
+export (float) var health
+export (float) var regen_per_second = 0
+var is_dead = false
+
+export (int) var verts = 0
 
 func _ready():
 	add_to_group(get_node("/root/Globals").EnemyGroupName)
+	
+	max_health = health
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	offset = offset + speed * delta
+	process_health(delta)
+	
+func take_damage(damage):
+	health -= damage
+	
+	if health <= 0:
+		is_dead = true
+		# emit death signal
+
+func process_health(delta):
+	if health < max_health:
+		health += regen_per_second * delta
+	else:
+		health = max_health
