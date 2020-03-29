53c09b2f2a81e0b662a7e81c3bf2d31d10f2bb10
Chris bruno <chris234k@gmail.com>
2020-03-29 13:09:54 -0400

    Hiding sprite on death

--- AngelsVsEllipsis/enemies/Enemy.gd
+++ AngelsVsEllipsis/enemies/Enemy.gd
@@ -1,14 +1,20 @@
 extends Node2D
 
+
 export (float) var move_speed = 50
 
 var max_health
 export (float) var health
+
 export (float) var regen_per_second = 0
+
+onready var sprite = $Sprite
+
 var is_dead = false
 signal death(verts)
 
 export (int) var verts = 0
+
 
 func _ready():
 	add_to_group(get_node("/root/Globals").EnemyGroupName)
@@ -18,17 +24,22 @@ func _ready():
 func _process(delta):
 	if !is_dead:
 		process_health(delta)
-	
-func take_damage(damage):
-	health -= damage
-	
-	if health <= 0:
-		if !is_dead:
-			is_dead = true
-			emit_signal("death", verts)
 
 func process_health(delta):
 	if health < max_health:
 		health += regen_per_second * delta
 	else:
 		health = max_health
+	
+func take_damage(damage):
+	health -= damage
+	
+	if health <= 0:
+		if !is_dead:
+			die()
+
+func die():
+	is_dead = true
+	sprite.visible = false
+	
+	emit_signal("death", verts)
